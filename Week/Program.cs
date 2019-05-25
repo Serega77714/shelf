7cd5676596c55027d3b739104f38b310d2d92563
serega77714 <serega77714@mail.ru>
2019-05-26 01:33:58 +0300

    second attempt

---
+++ Week/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string con = "Y";
+            for (;con == "Y" || con == "y";)
+            {
+                Console.WriteLine("Enter the number of the day of the week");
+                string number = Console.ReadLine();
+                Console.WriteLine(Week(number));
+                Console.WriteLine("Will you continue?-Y/N");
+                con = Console.ReadLine();
+             }
+                           
+        }
+
+              static string Week(string number)
+        {
+            
+                switch (number)
+                {
+                    case "1": return "Monday";
+                        break;
+                    case "2":
+                    return "Tuesday";
+                        break;
+                    case "3":
+                    return "Wednesday";
+                        break;
+                    case "4":
+                    return "Thursday";
+                        break;
+                    case "5":
+                    return "Friday";
+                        break;
+                    case "6":
+                    return "Saturday";
+                        break;
+                    case "7":
+                    return"Sunday";
+                        break;
+                   default:
+                    return "Entered incorrect day of week ";
+                    break;
+                }
+           
+                
+        }
+    }
+}
---
+++ multiplication table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insert the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Mult(num);
+            Console.ReadKey();
+        }
+
+        static void Mult(int num)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                int a = num * i;
+                Console.WriteLine($"{num}*{i}={a}");
+                
+            }
+        }
+            }
+}
